c1c395888df01825ec875cddf81e7e8189c07974
unknown <ivanalbano9@gmail.com>
2024-12-01 18:23:56 +0200

    Initial Setup

---
+++ guinxa-pass/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  variable: "--font-poppins",
+  weight: ['400', '500', '600', '700']
+});
+
+
+export const metadata: Metadata = {
+  title: "nkuvuPass",
+  description: "Nkuvu is a platform for event management",
+  icons: { icon: '\assets\images\logo.svg' }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={poppins.variable}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
